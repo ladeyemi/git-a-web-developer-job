462772b06c4ff0b313a6738708d771b45a302620
ladeyemi <larryadeyemi@gmail.com>
2017-03-30 07:58:14 +0100

    new files and some tweaks

---
+++ gulp/tasks/watch.js
@@ -0,0 +1,30 @@
+var gulp = require('gulp'),
+watch = require('gulp-watch'),
+browserSync = require ('browser-sync').create(); /* browserSync is pretty amazing can be used for cross browser testing and also mobile testing using the external link generated!! Also very nice to be able to update CSS without need of a reload */
+
+
+
+gulp.task('watch', function() {
+browserSync.init ({
+notify: false,   /* (Can turn on/off injection with this) */
+  server: {
+    baseDir: "app"
+  }
+});
+
+watch('./app/index.html', function() {
+browserSync.reload();
+
+});
+
+watch('./app/assets/styles/**/*.css', function() {
+gulp.start('cssInject');
+
+});
+
+});
+
+gulp.task ('cssInject', ['styles'],function() {
+  return gulp.src('./app/temp/styles/styles.css')
+  .pipe(browserSync.stream());
+});
