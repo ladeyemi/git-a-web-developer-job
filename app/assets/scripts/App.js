3f0d6094879abe1ac68fb0b03251a96b86c17256
ladeyemi <larryadeyemi@gmail.com>
2017-04-26 13:26:58 +0100

    sticky header complete

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -4,7 +4,11 @@ import RevealOnScroll from './modules/RevealOnScroll';
 
 import $ from 'jquery';
 
+import StickyHeader from './modules/StickyHeader';
+
 var mobileMenu = new MobileMenu();
 
 new RevealOnScroll($(".feature-item"), "85%");
 new RevealOnScroll($(".testimonial"), "60%");
+
+var stickyHeader = new StickyHeader();
