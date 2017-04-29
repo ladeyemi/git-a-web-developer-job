c40180d86d3a9229cdd8739ddc6d48bc0993ab2f
ladeyemi <larryadeyemi@gmail.com>
2017-04-29 10:57:34 +0100

    recent changes

--- app/assets/scripts/modules/Modal.js
+++ app/assets/scripts/modules/Modal.js
@@ -1,21 +1,21 @@
 import $ from 'jquery';
 
 class Modal {
-  constructor () {
-    this.openModalButton = $('.open-modal');
-    this.modal= $('.modal');
-    this.closeModalButton = $('.modal__close');
+  constructor() {
+    this.openModalButton = $(".open-modal");
+    this.modal = $(".modal");
+    this.closeModalButton = $(".modal__close");
     this.events();
   }
 
   events() {
+    // clicking the open modal button
     this.openModalButton.click(this.openModal.bind(this));
-    //clicking the open modal button
-    //
-    //clicking the x close modal button
+
+    // clicking the x close modal button
     this.closeModalButton.click(this.closeModal.bind(this));
 
-    //pushes any key
+    // pushes any key
     $(document).keyup(this.keyPressHandler.bind(this));
   }
 
@@ -23,16 +23,14 @@ class Modal {
     if (e.keyCode == 27) {
       this.closeModal();
     }
-
   }
 
-  openModal () {
+  openModal() {
     this.modal.addClass("modal--is-visible");
     return false;
-
   }
 
-  closeModal () {
+  closeModal() {
     this.modal.removeClass("modal--is-visible");
   }
 }
