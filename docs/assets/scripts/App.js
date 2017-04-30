0f7862901416333a08e13a4a91dbaa38ac702b93
ladeyemi <larryadeyemi@gmail.com>
2017-04-30 15:57:06 +0100

    Completed gulp build task

---
+++ docs/assets/scripts/App.js
@@ -0,0 +1,17 @@
+import MobileMenu from './modules/MobileMenu';
+
+import RevealOnScroll from './modules/RevealOnScroll';
+
+import Modal from './modules/Modal';
+
+import $ from 'jquery';
+
+import StickyHeader from './modules/StickyHeader';
+
+var mobileMenu = new MobileMenu();
+
+new RevealOnScroll($(".feature-item"), "85%");
+new RevealOnScroll($(".testimonial"), "60%");
+
+var stickyHeader = new StickyHeader();
+var modal = new Modal();
